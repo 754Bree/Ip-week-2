4213423f7b0cb95aa2990733b139ea8dc5fab929
Briana <briana.odhiambo@student.moringaschool.com>
2021-11-16 14:33:04 +0300

    Add changes to document

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,24 @@
 // (back-end logic):
 var akan = function(y,m,d,g) {
-  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  var femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"];
-  var d = date(y,m,d);
-  if (g==="Male"){
-    return d && maleNames[d.getDay()];
-  } else {
-    return d && femaleNames[d.getDay()];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"];
+    var d = date(y,m,d);
+    if  (g==="Male"){
+      return d && maleNames[d.getDay()];
+  }   else {
+      return d && femaleNames[d.getDay()];
   }
 }
+//(front-end):
+  $(document).ready(function() {
+    $("form#form").submit(function(event){
+    event.preventDefault();
+    var y = parseInt($("#Year").val());
+    var m = parseInt($("Month").val());
+    var d = parseInt($("Date").val());
+    var g = $("input:radio[name=gender]:checked").val();
+    var result =akan (y, m,d,g);
+    alert("your Ghanian name is: "+ result);
+    document.get ElementById("form").reset();
+  });
+});
