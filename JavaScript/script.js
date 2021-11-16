8834df3a51a1dab687ab5cffb1305a5e38e1e5be
Briana <briana.odhiambo@student.moringaschool.com>
2021-11-16 11:47:11 +0300

    Add files

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,11 @@
-v
+// (back-end logic):
+var akan = function(y,m,d,g) {
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"];
+  var d = date(y,m,d);
+  if (g==="Male"){
+    return d && maleNames[d.getDay()];
+  } else {
+    return d && femaleNames[d.getDay()];
+  }
+}
